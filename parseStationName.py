dcdaceada52e7340bb3a384c08ddf6629e67cf66
李燕鹏 <liyp@primeton.com>
2017-12-22 15:22:44 +0800

    Update: 学习用Vscode来写python

--- parseStationName.py
+++ parseStationName.py
@@ -3,11 +3,12 @@
 import re
 import requests
 
-url = "https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9035"
-response = requests.get(url)
-stations = re.findall(r'[\u4e00-\u9fa5]+\|[A-Z]+',response.text)
+URL = "https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9035"
+resp = requests.get(URL)
+stations = re.findall(r'[\u4e00-\u9fa5]+\|[A-Z]+', resp.text)
 stationsInfo = {}
 for station in stations:
-    key = station.split('|',1)[0]
-    value = station.split('|',1)[1]
-    stationsInfo[key]=value
+    key = station.split('|', 1)[0]
+    value = station.split('|', 1)[1]
+    stationsInfo[key] = value
+    
